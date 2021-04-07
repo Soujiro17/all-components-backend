8f9724a18a543687c763c3a8cdab38a69984f193
Soujiro17 <vicente_tomi@hotmail.com>
2021-04-07 17:43:45 -0400

    get especific and put

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.set('json spaces', 2)
 
 // Middlewares
 app.use(morgan('dev'))
-app.use(express.json({ limit: "30mb", extended: true}));
-app.use(express.urlencoded({ limit: "30mb", extended: true}));
+app.use(express.json({ limit: "50mb", extended: true}));
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000}));
 app.use(cors());
 
 // Routes
--- routes/data-api.js
+++ routes/data-api.js
@@ -14,6 +14,39 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try{
+
+        const id = req.params.id
+
+        const product = await Product.findById(id)
+
+        res.status(200).json(product)
+
+    }catch(err){
+        res.status(400).json({status: 400, description: err})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    
+    try{
+        
+        const id = req.params.id;
+        const data = req.body.data;
+
+        const product = await Product.findByIdAndUpdate(id, data);
+
+        await product.save();
+
+        res.status(201).json({status: 201, description: 'Product updated success'});
+    }catch(err){
+        console.log(err)
+        res.status(409).json({status: 409, description: err})
+    }
+
+})
+
 router.post('/', async (req, res) => {
     
     try{
