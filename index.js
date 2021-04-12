a8ed91d90e2821fbaf7af650269fb865912c79f9
Soujiro17 <vicente_tomi@hotmail.com>
2021-04-12 11:15:55 -0400

    a

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: "50mb", extended: true}));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000}));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({credentials:  true }));
 
 // Routes
 app.use('/api/data', require('./routes/dataRouter.js'));
