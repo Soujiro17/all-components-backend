bdb64f53af1f411d4eb2fd05648daf2dd2e0bedc
Soujiro17 <vicente_tomi@hotmail.com>
2021-04-12 15:56:05 -0400

    y ahora

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.set('json spaces', 2);
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(cookieParser());
 app.use(express.json({ limit: "50mb", extended: true}));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 1000000}));
-app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:3000", "https://all-components-pc.netlify.app", "https://all-components-pc.netlify.app/"], credentials:  true }));
 
 // Routes
